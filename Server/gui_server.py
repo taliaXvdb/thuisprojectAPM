9a55d6f4e9b8c3781510b80a7d487a439440a7c5
taliaXvdb <talia.van.den.bossche@student.howest.be>
2024-04-22 14:50:13 +0200

    feat: copy gui server

---
+++ Server/gui_server.py
@@ -0,0 +1,78 @@
+# https://pythonprogramming.net/python-3-tkinter-basics-tutorial/
+import logging
+import socket
+from queue import Queue
+from threading import Thread
+from tkinter import *
+
+from clientserver6.server.server import SommenServer
+
+
+class ServerWindow(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+        self.server = None
+        self.thread_listener_queue=None
+        self.init_messages_queue()
+
+
+
+    # Creation of init_window
+    def init_window(self):
+        # changing the title of our master widget
+        self.master.title("Server")
+
+        # allowing the widget to take the full space of the root window
+        self.pack(fill=BOTH, expand=1)
+
+        Label(self, text="Log-berichten server:").grid(row=0)
+        self.scrollbar = Scrollbar(self, orient=VERTICAL)
+        self.lstnumbers = Listbox(self, yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.lstnumbers.yview)
+
+        self.lstnumbers.grid(row=1, column=0, sticky=N + S + E + W)
+        self.scrollbar.grid(row=1, column=1, sticky=N + S)
+
+        self.btn_text = StringVar()
+        self.btn_text.set("Start server")
+        self.buttonServer = Button(self, textvariable=self.btn_text, command=self.start_stop_server)
+        self.buttonServer.grid(row=3, column=0, columnspan=2, pady=(5, 5), padx=(5, 5), sticky=N + S + E + W)
+
+        Grid.rowconfigure(self, 1, weight=1)
+        Grid.columnconfigure(self, 0, weight=1)
+
+
+    def start_stop_server(self):
+        if self.server is not None:
+            self.__stop_server()
+        else:
+            self.__start_server()
+
+    def __stop_server(self):
+        self.server.stop_server()
+        self.server = None
+        logging.info("Server stopped")
+        self.btn_text.set("Start server")
+
+    def __start_server(self):
+        self.server = SommenServer(socket.gethostname(), 9999, self.messages_queue)
+        self.server.init_server()
+        self.server.start()  # in thread plaatsen!
+        logging.info("Server started")
+        self.btn_text.set("Stop server")
+
+    def init_messages_queue(self):
+        self.messages_queue = Queue()
+        self.thread_listener_queue = Thread(target=self.print_messsages_from_queue, name="Queue_listener_thread", daemon=True)
+        self.thread_listener_queue.start()
+
+    def print_messsages_from_queue(self):
+        message = self.messages_queue.get()
+        while message != "CLOSE_SERVER":
+            self.lstnumbers.insert(END, message)
+            self.messages_queue.task_done()
+            message = self.messages_queue.get()
+
+
