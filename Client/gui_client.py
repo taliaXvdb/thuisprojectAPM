48296e6ea7126b60ef975e8dc224901e1089b6a0
taliaXvdb <talia.van.den.bossche@student.howest.be>
2024-04-27 18:14:55 +0200

    feat: tabs voor elke zoekopdracht

--- Client/gui_client.py
+++ Client/gui_client.py
@@ -1,82 +1,63 @@
-import sys
-from pathlib import Path
-print(sys.path[0])
-sys.path[0] = str(Path(sys.path[0]).parent.parent)
-print(sys.path[0])
-
-
-
-# https://pythonprogramming.net/python-3-tkinter-basics-tutorial/
 import logging
 import socket
 import pickle
-from tkinter import *
-
-from tkinter import messagebox
-from tkinter.ttk import Combobox
+import tkinter as tk
+from tkinter import messagebox, ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import pandas as pd
 
 
-class Window(Frame):
+class Window(tk.Frame):
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        super().__init__(master)
         self.master = master
         self.init_window()
-        self.makeConnnectionWithServer()
+        self.make_connection_with_server()
 
-    # Creation of init_window
     def init_window(self):
-        # changing the title of our master widget
         self.master.title("Login")
+        self.pack(fill=tk.BOTH, expand=1)
 
-        # allowing the widget to take the full space of the root window
-        self.pack(fill=BOTH, expand=1)
+        tk.Label(self, text="Naam:").grid(row=0)
+        tk.Label(self, text="Wachtwoord:", pady=10).grid(row=1)
 
-        Label(self, text="Naam:").grid(row=0)
-        Label(self, text="Wachtwoord:", pady=10).grid(row=1)
+        self.entry_naam = tk.Entry(self, width=40)
+        self.entry_wachtwoord = tk.Entry(self, width=40, show="*")
 
-        self.entry_naam = Entry(self, width=40)
-        self.entry_wachtwoord = Entry(self, width=40, show="*")
+        self.entry_naam.grid(row=0, column=1, sticky=tk.E + tk.W, padx=(5, 5), pady=(5, 5))
+        self.entry_wachtwoord.grid(row=1, column=1, sticky=tk.E + tk.W, padx=(5, 5), pady=(5, 0))
 
-        self.entry_naam.grid(row=0, column=1, sticky=E + W, padx=(5, 5), pady=(5, 5))
-        self.entry_wachtwoord.grid(row=1, column=1, sticky=E + W, padx=(5, 5), pady=(5, 0))
+        self.button_login = tk.Button(self, text="Login", command=self.login)
+        self.button_login.grid(row=3, column=0, columnspan=2, pady=(0, 5), padx=(5, 5), sticky=tk.N + tk.S + tk.E + tk.W)
 
-        self.buttonCalculate = Button(self, text="Login", command=self.login)
-        self.buttonCalculate.grid(row=3, column=0, columnspan=2, pady=(0, 5), padx=(5, 5), sticky=N + S + E + W)
-
-        Grid.rowconfigure(self, 3, weight=1)
-        Grid.columnconfigure(self, 1, weight=1)
+        tk.Grid.rowconfigure(self, 3, weight=1)
+        tk.Grid.columnconfigure(self, 1, weight=1)
 
     def __del__(self):
         self.close_connection()
 
-    def makeConnnectionWithServer(self):
+    def make_connection_with_server(self):
         try:
             logging.info("Making connection with server...")
-            # get local machine name
             host = socket.gethostname()
             port = 9999
             self.socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # connection to hostname on the port.
             self.socket_to_server.connect((host, port))
-            # NIEUW
             self.in_out_server = self.socket_to_server.makefile(mode='rwb')
-            logging.info("Open connection with server succesfully")
+            logging.info("Open connection with server successfully")
         except Exception as ex:
-            logging.error(f"Foutmelding: {ex}")
-            messagebox.showinfo("Foutmelding", "Something has gone wrong...")
-
+            logging.error(f"Error: {ex}")
+            messagebox.showinfo("Error", "Something has gone wrong...")
 
     def close_connection(self):
         try:
-            logging.info("Close connection with server...")
+            logging.info("Closing connection with server...")
             pickle.dump("CLOSE", self.in_out_server)
             self.in_out_server.flush()
             self.socket_to_server.close()
         except Exception as ex:
-            logging.error("Foutmelding:close connection with server failed")
+            logging.error("Error: Failed to close connection with server")
 
     def login(self):
         naam = self.entry_naam.get()
@@ -86,100 +67,94 @@ class Window(Frame):
             login_to_send = pickle.dumps(("LOGIN", (naam, wachtwoord)))
             pickle.dump(login_to_send, self.in_out_server)
             self.in_out_server.flush()
-            logging.info("Login data sent to server succesfully")
+            logging.info("Login data sent to server successfully")
 
             logging.info("Waiting for response from server...")
 
             commando = pickle.load(self.in_out_server)
-            logging.info(f"Commando received: {commando}")
+            logging.info(f"Command received: {commando}")
             if commando == "OK":
-                result = messagebox.showinfo("Login", "Login succesful")
+                result = messagebox.showinfo("Login", "Login successful")
                 if result == "ok":
                     self.show_main_window()
             else:
                 messagebox.showinfo("Login", "Login failed")
         except Exception as ex:
-            logging.error(f"Foutmelding Client: {ex}")
-            messagebox.showinfo("Foutmelding", "Something has gone wrong...")
+            logging.error(f"Error: {ex}")
+            messagebox.showinfo("Error", "Something has gone wrong...")
 
     def show_main_window(self):
         logging.info("Close current window")
-
         for widget in self.winfo_children():
             widget.destroy()
         
-        #make new window
         self.master.title("Main")
+        self.pack(fill=tk.BOTH, expand=1)
 
-        self.pack(fill=BOTH, expand=1)
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(fill=tk.BOTH, expand=1)
 
-        Label(self, text="Main window").grid(row=0)
-        Label(self, text="Search:", pady=10).grid(row=1)
+        # Define a list of all possible searches
+        searches = [
+            "Grafiek van de grootte van de appels",
+            "Heeft de appel met deze rijpheid een goede kwaliteit",
+            "Hoe zoet is de appel met deze zuurtegraad",
+            "Hoe krokant is een appel met deze rijpheid"
+        ]
 
-        # dropdown with possible searches
-        self.combobox_searches = Combobox(self)
-        self.combobox_searches['values'] = ["Grafiek van de grootte van de appels", "Heeft de appel met deze rijpheid een goede kwaliteit", "Hoe zoet is de appel met deze zuurtegraad", "Hoe krokant is een appel met deze rijpheid"]
-        self.combobox_searches.current(0)
+        # Create tabs for each search
+        for search in searches:
+            tab = ttk.Frame(self.notebook)
+            self.notebook.add(tab, text=search)
 
-        self.combobox_searches.grid(row=1, column=1, sticky=E + W, padx=(5, 5), pady=(5, 5))
+        # Bind the tab changed event
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_changed)
 
-        self.buttonCalculate = Button(self, text="Search", command=self.search)
-
-        self.buttonCalculate.grid(row=3, column=0, columnspan=2, pady=(0, 5), padx=(5, 5), sticky=N + S + E + W)
-
-        Grid.rowconfigure(self, 3, weight=1)
-        Grid.columnconfigure(self, 1, weight=1)
-
-    def search(self):
-        search = self.combobox_searches.get()
-        messagebox.showinfo("Search", f"Searching for {search}...")
-
+    def on_tab_changed(self, event):
         try:
-            if hasattr(self, 'plot_canvas'):
-                self.plot_canvas.get_tk_widget().destroy()
-            logging.info("Sending search data to server...")
-            search_to_send = pickle.dumps(("SEARCH", search))
+            selected_tab_index = event.widget.index("current")
+            selected_tab_text = event.widget.tab(selected_tab_index, "text")
+            logging.info(f"Selected tab: {selected_tab_text}")
+            search_to_send = pickle.dumps(("SEARCH", selected_tab_text))
             pickle.dump(search_to_send, self.in_out_server)
             self.in_out_server.flush()
-            logging.info("Search data sent to server succesfully")
-
+            logging.info("Search data sent to server successfully")
             logging.info("Waiting for response from server...")
 
             commando = pickle.load(self.in_out_server)
-            logging.info(f"Commando received: {commando}")
+            logging.info(f"Command received: {commando}")
             if commando == "OK":
-                messagebox.showinfo("Search", "Search succesful")
-                self.show_search(search)
+                self.show_search_result(selected_tab_text)
             else:
                 messagebox.showinfo("Search", "Search failed")
         except Exception as ex:
-            logging.error(f"Foutmelding Client: {ex}")
-            messagebox.showinfo("Foutmelding", "Something has gone wrong...")
+            logging.error(f"Error: {ex}")
+            messagebox.showinfo("Error", "Something has gone wrong...")
 
-    def show_search(self, search):
+    def show_search_result(self, search):
         if search == "Grafiek van de grootte van de appels":
             data = pd.read_csv('../Data/apple_quality.csv')
-            self.plot_graph(data)
+            tab = ttk.Frame(self.notebook)
+            self.notebook.add(tab, text=search)
+            self.plot_graph(data, tab)
 
-    
-    def plot_graph(self, data):
+    def plot_graph(self, data, parent):
         fig = Figure(figsize=(10, 6), dpi=100)
         plot = fig.add_subplot(1, 1, 1)
 
-        # Plot each column against the 'A_id' column
-        for column in data.columns[1:-1]:  # Exclude 'A_id' and 'Quality'
+        for column in data.columns[1:-1]:  
             plot.plot(data['A_id'], data['Size'], marker='o', label=column)
 
         plot.set_xlabel('Id')
         plot.set_ylabel('Size')
 
-        self.plot_canvas = FigureCanvasTkAgg(fig, master=root)
-        self.plot_canvas.draw()
-        self.plot_canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+        canvas = FigureCanvasTkAgg(fig, master=parent)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
 
 logging.basicConfig(level=logging.INFO)
 
-root = Tk()
-# root.geometry("400x300")
+root = tk.Tk()
 app = Window(root)
 root.mainloop()
