737cc609cb262c0fc38cfaa01e5f8d48f375f53b
taliaXvdb <talia.van.den.bossche@student.howest.be>
2024-04-28 14:54:27 +0200

    feat: tabs in serverside

--- Server/gui_server.py
+++ Server/gui_server.py
@@ -5,6 +5,7 @@ import socket
 from queue import Queue
 from threading import Thread, enumerate
 from tkinter import *
+from tkinter import messagebox, ttk
 import sys
 from pathlib import Path
 print(sys.path[0])                  #test
@@ -29,21 +30,20 @@ class ServerWindow(Frame):
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
 
-        Label(self, text="Log-berichten server:").grid(row=0)
-        self.scrollbar = Scrollbar(self, orient=VERTICAL)
-        self.lstnumbers = Listbox(self, yscrollcommand=self.scrollbar.set)
-        self.scrollbar.config(command=self.lstnumbers.yview)
+        self.top_bar = Frame(self)  # Frame for the top bar
+        self.top_bar.pack(side=TOP, fill=X)
 
-        self.lstnumbers.grid(row=1, column=0, sticky=N + S + E + W)
-        self.scrollbar.grid(row=1, column=1, sticky=N + S)
+        self.title = Label(self.top_bar, text="Server Application")
+        self.title.pack(side=LEFT, padx=(5, 5))
 
         self.btn_text = StringVar()
         self.btn_text.set("Start server")
-        self.buttonServer = Button(self, textvariable=self.btn_text, command=self.start_stop_server)
-        self.buttonServer.grid(row=3, column=0, columnspan=2, pady=(5, 5), padx=(5, 5), sticky=N + S + E + W)
+        self.buttonServer = Button(self.top_bar, textvariable=self.btn_text, command=self.start_stop_server)
+        self.buttonServer.pack(side=RIGHT, padx=(5, 5))
 
-        Grid.rowconfigure(self, 1, weight=1)
-        Grid.columnconfigure(self, 0, weight=1)
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(fill=BOTH, expand=1)
+
 
     def start_stop_server(self):
         if self.server is not None:
@@ -63,6 +63,7 @@ class ServerWindow(Frame):
         self.server.start()  # in thread plaatsen!
         logging.info("Server started")
         self.btn_text.set("Stop server")
+        self.show_tabs()
 
     def init_messages_queue(self):
         self.messages_queue = Queue()
@@ -76,4 +77,63 @@ class ServerWindow(Frame):
             self.messages_queue.task_done()
             message = self.messages_queue.get()
 
+    def show_tabs(self):
+        self.tab1 = Frame(self.notebook)
+        self.notebook.add(self.tab1, text="Logs")
+        self.notebook.pack(fill=BOTH, expand=1)
 
+        self.lstnumbers = Listbox(self.tab1)
+        self.lstnumbers.pack(fill=BOTH, expand=1)
+
+        self.tab2 = Frame(self.notebook)
+        self.notebook.add(self.tab2, text="Popularity")
+        self.notebook.pack(fill=BOTH, expand=1)
+
+        self.tab3 = Frame(self.notebook)
+        self.notebook.add(self.tab3, text="Online users")
+        self.notebook.pack(fill=BOTH, expand=1)
+
+        self.tab4 = Frame(self.notebook)
+        self.notebook.add(self.tab4, text="Userbase")
+        self.notebook.pack(fill=BOTH, expand=1)
+
+        self.tab5 = Frame(self.notebook)
+        self.notebook.add(self.tab5, text="Message")
+        self.notebook.pack(fill=BOTH, expand=1)
+
+        self.lstusers = Listbox(self.tab2)
+        self.lstusers.pack(fill=BOTH, expand=1)
+
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_changed)
+
+    def on_tab_changed(self, event):
+        tab_id = event.widget.select()
+        tab_name = event.widget.tab(tab_id, "text")
+        print(tab_name)
+
+        if tab_name == "Logs":
+            self.print_messsages_from_queue()
+
+        elif tab_name == "Popularity":
+            self.show_popularity()
+
+        elif tab_name == "Online users":
+            self.show_online_users()
+
+        elif tab_name == "Userbase":
+            self.show_userbase()
+
+        elif tab_name == "Message":
+            self.send_message()
+
+    def show_popularity(self):
+        pass
+
+    def show_online_users(self):
+        pass
+
+    def show_userbase(self):
+        pass
+
+    def send_message(self):
+        pass
